c2d10f137a181a6c5940240ed958469b311a25c1
Diego barbosa da silva <98347866+diegop42@users.noreply.github.com>
2024-08-20 14:25:58 -0300

    Add files via upload

--- bytebank-Poo/index.js
+++ bytebank-Poo/index.js
@@ -1,16 +1,17 @@
 import { Cliente } from "./Cliente.js";
-import { ContaCorrente } from "./ContaCorrente.js";
+import { Gerente } from "./Funcionario/Gerente.js";
+import { Diretor } from "./Funcionario/Diretor.js";
+import { SistemaAutenticacao } from "./SistemaAutenticacao.js";
 
-const cliente1 = new Cliente("Ricardo", 11122233309);
-const cliente2 = new Cliente("alice", 88822233309);
+const diretor = new Diretor("Rodrigo", 10000, 12345678900);
+diretor.cadastrarSenha("123456");
+const gerente = new Gerente("Ricardo", 5000, 12378945601);
+gerente.cadastrarSenha("123");
 
-const ContaCorrenteRicardo = new ContaCorrente(101, cliente1);
-ContaCorrenteRicardo.depositar(500);
-const conta2 = new ContaCorrente(102, cliente2);
+const cliente = new Cliente("Lais", 78945612379, "456");
+const gerenteEstaLogado = SistemaAutenticacao.login(gerente, "123");
+const diretorEstaLogado = SistemaAutenticacao.login(diretor, "123456");
 
+const clienteEstaLogado = SistemaAutenticacao.login(cliente, "456");
 
-
-let valor = 200;
-ContaCorrenteRicardo.transferir(valor, conta2);
-
-console.log(ContaCorrente.numeroDecontas)
+console.log(gerenteEstaLogado, diretorEstaLogado, clienteEstaLogado);
